a71ee6b2785a1bd9af200f6a9e6dca18f6019e2a
Damián Pumar <damianpumar@gmail.com>
2022-10-31 17:28:55 +0100

    🚑 Removed unused empty line

--- src/infrastructure/adapter/useAddToCartUseCase.ts
+++ src/infrastructure/adapter/useAddToCartUseCase.ts
@@ -4,7 +4,6 @@ import { useNotifier } from "../services/NotificationService";
 import { useCartStorage } from "../services/CartStorageService";
 import { AddToCartUseCase } from "../../application/add-to-cart";
 
-
 export const useAddToCart = () => {
     const storage: CartStorageService = useCartStorage();
     const notifier: NotificationService = useNotifier();
