38c4d272897d072009213056daa68ea298ccaa2b
Damián Pumar <damianpumar@gmail.com>
2022-10-31 12:42:00 +0100

    ✨ Installed dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
 import "./index.css";
 
+import { DIProvider } from "@codescouts/ui";
 import ReactDOM from "react-dom/client";
+
 import reportWebVitals from "./reportWebVitals";
 
 import App from "./App";
 
-import { DIProvider } from "./DIProvider";
+import { Provider } from "./Provider";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
-  <DIProvider>
+  <Provider>
     <App />
-  </DIProvider>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
