d8c8e1fcb9eeeae2e3404b8b2d6c278982327aa2
Damián Pumar <damianpumar@gmail.com>
2023-02-20 15:53:47 +0100

    ✨ Add unit test

---
+++ src/application/add-to-cart.test.ts
@@ -0,0 +1,52 @@
+import { MockProxy, mock } from "jest-mock-extended"
+
+import { CartStorageService } from "../domain/services/CartStorageService"
+import { NotificationService } from "../domain/services/NotificationService"
+
+import { AddToCartUseCase } from "./add-to-cart"
+
+import { User } from "../domain/model/user"
+import { Product } from "../domain/model/product"
+import { Cart } from "../domain/model/cart"
+
+describe("AddToCart should", () => {
+    const cartMock = mock<Cart>();
+    const cartStorage = mock<CartStorageService>({
+        cart: cartMock
+    });
+    const notifier = mock<NotificationService>();
+
+    test("show message when user has allergies to product", () => {
+
+        const user = new User("FAKE", "FAKE", "FAKE", [], [
+            "cacahuete",
+            "cacao"
+        ], []);
+
+        const productWithAllergies = new Product("FAKE", "FAKE", 0, ["cacahuete"]);
+
+        const addToProduct = new AddToCartUseCase(cartStorage, notifier);
+
+        addToProduct.execute(user, productWithAllergies);
+
+        expect(notifier.notify).toHaveBeenCalledWith("¡Esta cookie es peligrosa para su salud! 😱");
+        expect(cartMock.addProduct).toBeCalledTimes(0);
+    })
+
+    test("add product to cart", () => {
+        const user = new User("FAKE", "FAKE", "FAKE", [], [
+            "cacahuete",
+            "cacao"
+        ], []);
+
+        const productWithNoAllergies = new Product("FAKE", "FAKE", 0, ["malvavisco"]);
+
+        const addToProduct = new AddToCartUseCase(cartStorage, notifier);
+
+        addToProduct.execute(user, productWithNoAllergies);
+
+        expect(notifier.notify).toBeCalledTimes(0);
+        expect(cartMock.addProduct).toHaveBeenCalledWith(productWithNoAllergies)
+        expect(cartStorage.updateCart).toHaveBeenCalledWith(cartMock);
+    })
+})
--- src/application/authenticate.ts
+++ src/application/authenticate.ts
@@ -14,5 +14,3 @@ export class AuthenticateUseCase {
     this.userStorageService.updateUser(user);
   }
 }
-
-
